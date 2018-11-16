cb6bb052717fc600facf1a02ed7f62f11673fee2
xilinxia <511598384@qq.com>
2018-11-16 11:14:09 +0800

    login修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -74,7 +74,7 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: '1111111'
+        password: 'admin'
       },
       loginRules: {
         username: [
