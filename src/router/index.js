ef3f3ff62d5829e238d29b013b34e4e6008088fb
yc <434848237@qq.com>
2019-01-06 19:52:12 +0800

    菜单：绑定id用于鉴权

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Layout from '@/views/layout/Layout'
  *  detail see  https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
  **/
 
+// noinspection JSAnnotator
 /**
  * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
  * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
@@ -71,7 +72,7 @@ export const constantRouterMap = [
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         name: 'Dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+        meta: { title: 'dashboard', icon: 'dashboard', noCache: true, id: 2 }
       }
     ]
   },
@@ -85,7 +86,7 @@ export const constantRouterMap = [
         path: 'index',
         component: () => import('@/views/guide/index'),
         name: 'Guide',
-        meta: { title: 'guide', icon: 'guide', noCache: true }
+        meta: { title: 'guide', icon: 'guide', noCache: true, id: 4 }
       }
     ]
   },
@@ -100,7 +101,7 @@ export const constantRouterMap = [
         path: 'index',
         component: () => import('@/views/personal/index'),
         name: 'Personal',
-        meta: { title: 'personal', icon: 'user', noCache: true }
+        meta: { title: 'personal', icon: 'user', noCache: true, id: 6 }
       }
     ]
   },
@@ -121,7 +122,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/organizationalManagement/notice/index'),
         name: 'notice',
-        meta: { title: 'notice', noCache: true }
+        meta: { title: 'notice', noCache: true, id: 8 }
       },
       // 部门管理
       {
@@ -129,7 +130,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/organizationalManagement/departmentManagement/index'),
         name: 'departmentManagement',
-        meta: { title: 'departmentManagement', noCache: true }
+        meta: { title: 'departmentManagement', noCache: true, id: 9 }
       },
       // 人员管理
       {
@@ -137,7 +138,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/organizationalManagement/personnelManagement/index'),
         name: 'personnelManagement',
-        meta: { title: 'personnelManagement', noCache: true }
+        meta: { title: 'personnelManagement', noCache: true, id: 10 }
       },
       // 管理办法
       {
@@ -145,7 +146,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/organizationalManagement/managementMethods/index'),
         name: 'managementMethods',
-        meta: { title: 'managementMethods', noCache: true }
+        meta: { title: 'managementMethods', noCache: true, id: 11 }
       }
     ]
   },
@@ -164,73 +165,74 @@ export const constantRouterMap = [
       {
         path: 'auditPlan',
         component: () =>
-          import('@/views/auditManagement/auditPlan/index'),
+            import('@/views/auditManagement/auditPlan/index'),
         name: 'auditPlan',
-        meta: { title: 'auditPlan', noCache: true }
+        meta: { title: 'auditPlan', noCache: true, id: 24 }
       },
       // 工作底稿
       {
         path: 'workManuscript',
         component: () =>
-          import('@/views/auditManagement/workManuscript/index'),
+            import('@/views/auditManagement/workManuscript/index'),
         name: 'workManuscript',
-        meta: { title: 'workManuscript', noCache: true }
+        meta: { title: 'workManuscript', noCache: true, id: 13
+        }
       },
       // 事实确认书
       {
         path: 'confirmation',
         component: () =>
-          import('@/views/auditManagement/confirmation/index'),
+        import('@/views/auditManagement/confirmation/index'),
         name: 'confirmation',
-        meta: { title: 'confirmation', noCache: true }
+        meta: { title: 'confirmation', noCache: true, id: 14 }
       },
       // 违规积分通知书
       {
         path: 'punishNotice',
         component: () =>
-          import('@/views/auditManagement/punishNotice/index'),
+        import('@/views/auditManagement/punishNotice/index'),
         name: 'punishNotice',
-        meta: { title: 'punishNotice', noCache: true }
+        meta: { title: 'punishNotice', noCache: true, id: 15 }
       },
       // 整改通知
       {
         path: 'rectifyNotice',
         component: () =>
-          import('@/views/auditManagement/rectifyNotice/index'),
+        import('@/views/auditManagement/rectifyNotice/index'),
         name: 'rectifyNotice',
-        meta: { title: 'rectifyNotice', noCache: true }
+        meta: { title: 'rectifyNotice', noCache: true, id: 16 }
       },
       // 整改报告
       {
         path: 'rectifyReport',
         component: () =>
-          import('@/views/auditManagement/rectifyReport/index'),
+        import('@/views/auditManagement/rectifyReport/index'),
         name: 'rectifyReport',
-        meta: { title: 'rectifyReport', noCache: true }
+        meta: { title: 'rectifyReport', noCache: true, id: 25 }
       },
       // 审计报告
       {
         path: 'auditReport',
         component: () =>
-          import('@/views/auditManagement/auditReports/main'),
+        import('@/views/auditManagement/auditReports/main'),
         name: 'auditReport',
-        meta: { title: 'auditReport', noCache: true }
+        meta: { title: 'auditReport', noCache: true, id: 26 }
       },
       // 积分表
       {
         path: 'integralTable',
         component: () =>
-          import('@/views/auditManagement/integralTable/index'),
+        import('@/views/auditManagement/integralTable/index'),
         name: 'integralTable',
-        meta: { title: 'integralTable', noCache: true }
+        meta: { title: 'integralTable', noCache: true, id: 27 }
       },
       // 统计分析
       {
         path: 'statisticalAnalysis',
         component: () =>
-          import('@/views/auditManagement/statisticalAnalysis/index'),
+        import('@/views/auditManagement/statisticalAnalysis/index'),
         name: 'statisticalAnalysis',
-        meta: { title: 'statisticalAnalysis', noCache: true }
+        meta: { title: 'statisticalAnalysis', noCache: true, id: 17 }
       }
     ]
   },
@@ -251,7 +253,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/systemManagement/dictionaryManagement/index'),
         name: 'dictionaryManagement',
-        meta: { title: 'dictionaryManagement', noCache: true }
+        meta: { title: 'dictionaryManagement', noCache: true, id: 19 }
       },
       // 登录管理
       {
@@ -259,7 +261,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/systemManagement/loginManagement/index'),
         name: 'loginManagement',
-        meta: { title: 'loginManagement', noCache: true }
+        meta: { title: 'loginManagement', noCache: true, id: 20 }
       },
       // 系统日志
       {
@@ -267,7 +269,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/systemManagement/systemLog/index'),
         name: 'systemLog',
-        meta: { title: 'systemLog', noCache: true }
+        meta: { title: 'systemLog', noCache: true, id: 23 }
       },
       // 菜单管理
       {
@@ -275,7 +277,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/systemManagement/menusManagement/index'),
         name: 'menusManagement',
-        meta: { title: 'menusManagement', noCache: true }
+        meta: { title: 'menusManagement', noCache: true, id: 21 }
       },
       // 权限管理
       {
@@ -283,7 +285,7 @@ export const constantRouterMap = [
         component: () =>
           import('@/views/systemManagement/powerManagement/index'),
         name: 'powerManagement',
-        meta: { title: 'powerManagement', noCache: true }
+        meta: { title: 'powerManagement', noCache: true, id: 22 }
       }
     ]
   },
@@ -309,201 +311,201 @@ export default new Router({
   routes: constantRouterMap
 })
 
-export const asyncRouterMap = [
-  // 控制台
-  {
-    path: '',
-    redirect: 'dashboard',
-    children: [
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
-      }
-    ]
-  },
-  // 引导页
-  {
-    path: '/guide',
-    redirect: '/guide/index',
-    children: [
-      {
-        path: 'index',
-        name: 'Guide',
-        meta: { title: 'guide', icon: 'guide', noCache: true }
-      }
-    ]
-  },
-  // 个人中心
-  {
-    path: '/personal',
-    redirect: '/personal/index',
-    children: [
-      // 个人信息
-      {
-        path: 'index',
-        name: 'Personal',
-        meta: { title: 'personal', icon: 'user', noCache: true }
-      }
-    ]
-  },
-  // 机构管理
-  {
-    path: '/organization',
-    redirect: '/organization/notice',
-    name: 'organization',
-    meta: {
-      title: 'organization',
-      icon: 'component'
-    },
-    children: [
-      // 通知通告
-      {
-        path: 'notice',
-        name: 'notice',
-        meta: { title: 'notice', noCache: true }
-      },
-      // 部门管理
-      {
-        path: 'departmentManagement',
-        name: 'departmentManagement',
-        meta: { title: 'departmentManagement', noCache: true }
-      },
-      // 人员管理
-      {
-        path: 'personnelManagement',
-        name: 'personnelManagement',
-        meta: { title: 'personnelManagement', noCache: true }
-      },
-      // 管理办法
-      {
-        path: 'managementMethods',
-        name: 'managementMethods',
-        meta: { title: 'managementMethods', noCache: true }
-      }
-    ]
-  },
-  // 稽核管理
-  {
-    path: '/audit',
-    redirect: '/auditManagement/workManuscript',
-    name: 'audit',
-    meta: {
-      title: 'audit',
-      icon: 'international'
-    },
-    children: [
-      // 稽核方案
-      {
-        path: 'auditPlan',
-        name: 'auditPlan',
-        meta: { title: 'auditPlan', noCache: true }
-      },
-      // 工作底稿
-      {
-        path: 'workManuscript',
-        name: 'workManuscript',
-        meta: { title: 'workManuscript', noCache: true }
-      },
-      // 确认书
-      {
-        path: 'confirmation',
-        name: 'confirmation',
-        meta: { title: 'confirmation', noCache: true }
-      },
-      // 违规积分通知书
-      {
-        path: 'punishNotice',
-        name: 'punishNotice',
-        meta: { title: 'punishNotice', noCache: true }
-      },
-      // 整改通知
-      {
-        path: 'rectifyNotice',
-        name: 'rectifyNotice',
-        meta: { title: 'rectifyNotice', noCache: true }
-      },
-      // 整改报告
-      {
-        path: 'rectifyReport',
-        name: 'rectifyReport',
-        meta: { title: 'rectifyReport', noCache: true }
-      },
-      // 审计报告
-      {
-        path: 'auditReport',
-        name: 'auditReport',
-        meta: { title: 'auditReport', noCache: true }
-      },
-      // 积分表
-      {
-        path: 'integralTable',
-        name: 'integralTable',
-        meta: { title: 'integralTable', noCache: true }
-      },
-      // 统计分析
-      {
-        path: 'statisticalAnalysis',
-        name: 'statisticalAnalysis',
-        meta: { title: 'statisticalAnalysis', noCache: true }
-      }
-    ]
-  },
-  // 系统管理
-  {
-    path: '/system',
-    redirect: '/system/dictionaryManagement',
-    name: 'system',
-    meta: {
-      title: 'system',
-      icon: 'example'
-    },
-    children: [
-      // 字典管理
-      {
-        path: 'dictionaryManagement',
-        name: 'dictionaryManagement',
-        meta: { title: 'dictionaryManagement', noCache: true }
-      },
-      // 登录管理
-      {
-        path: 'loginManagement',
-        name: 'loginManagement',
-        meta: { title: 'loginManagement', noCache: true }
-      },
-      // 系统日志
-      {
-        path: 'systemLog',
-        name: 'systemLog',
-        meta: { title: 'systemLog', noCache: true }
-      },
-      // 菜单管理
-      {
-        path: 'menusManagement',
-        name: 'menusManagement',
-        meta: { title: 'menusManagement', noCache: true }
-      },
-      // 权限管理
-      {
-        path: 'powerManagement',
-        name: 'powerManagement',
-        meta: { title: 'powerManagement', noCache: true }
-      }
-    ]
-  }
-  // icon
-  // {
-  //   path: '/icon',
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       name: 'Icons',
-  //       meta: { title: 'icons', icon: 'icon', noCache: true }
-  //     }
-  //   ]
-  // }
-
-  /** When your routing table is too long, you can split it into small modules**/
-  //
-  // { path: '*', redirect: '/404', hidden: true }
-]
+// export const asyncRouterMap = [
+//   // 控制台
+//   {
+//     path: '',
+//     redirect: 'dashboard',
+//     children: [
+//       {
+//         path: 'dashboard',
+//         name: 'Dashboard',
+//         meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+//       }
+//     ]
+//   },
+//   // 引导页
+//   {
+//     path: '/guide',
+//     redirect: '/guide/index',
+//     children: [
+//       {
+//         path: 'index',
+//         name: 'Guide',
+//         meta: { title: 'guide', icon: 'guide', noCache: true }
+//       }
+//     ]
+//   },
+//   // 个人中心
+//   {
+//     path: '/personal',
+//     redirect: '/personal/index',
+//     children: [
+//       // 个人信息
+//       {
+//         path: 'index',
+//         name: 'Personal',
+//         meta: { title: 'personal', icon: 'user', noCache: true }
+//       }
+//     ]
+//   },
+//   // 机构管理
+//   {
+//     path: '/organization',
+//     redirect: '/organization/notice',
+//     name: 'organization',
+//     meta: {
+//       title: 'organization',
+//       icon: 'component'
+//     },
+//     children: [
+//       // 通知通告
+//       {
+//         path: 'notice',
+//         name: 'notice',
+//         meta: { title: 'notice', noCache: true }
+//       },
+//       // 部门管理
+//       {
+//         path: 'departmentManagement',
+//         name: 'departmentManagement',
+//         meta: { title: 'departmentManagement', noCache: true }
+//       },
+//       // 人员管理
+//       {
+//         path: 'personnelManagement',
+//         name: 'personnelManagement',
+//         meta: { title: 'personnelManagement', noCache: true }
+//       },
+//       // 管理办法
+//       {
+//         path: 'managementMethods',
+//         name: 'managementMethods',
+//         meta: { title: 'managementMethods', noCache: true }
+//       }
+//     ]
+//   },
+//   // 稽核管理
+//   {
+//     path: '/audit',
+//     redirect: '/auditManagement/workManuscript',
+//     name: 'audit',
+//     meta: {
+//       title: 'audit',
+//       icon: 'international'
+//     },
+//     children: [
+//       // 稽核方案
+//       {
+//         path: 'auditPlan',
+//         name: 'auditPlan',
+//         meta: { title: 'auditPlan', noCache: true }
+//       },
+//       // 工作底稿
+//       {
+//         path: 'workManuscript',
+//         name: 'workManuscript',
+//         meta: { title: 'workManuscript', noCache: true }
+//       },
+//       // 确认书
+//       {
+//         path: 'confirmation',
+//         name: 'confirmation',
+//         meta: { title: 'confirmation', noCache: true }
+//       },
+//       // 违规积分通知书
+//       {
+//         path: 'punishNotice',
+//         name: 'punishNotice',
+//         meta: { title: 'punishNotice', noCache: true }
+//       },
+//       // 整改通知
+//       {
+//         path: 'rectifyNotice',
+//         name: 'rectifyNotice',
+//         meta: { title: 'rectifyNotice', noCache: true }
+//       },
+//       // 整改报告
+//       {
+//         path: 'rectifyReport',
+//         name: 'rectifyReport',
+//         meta: { title: 'rectifyReport', noCache: true }
+//       },
+//       // 审计报告
+//       {
+//         path: 'auditReport',
+//         name: 'auditReport',
+//         meta: { title: 'auditReport', noCache: true }
+//       },
+//       // 积分表
+//       {
+//         path: 'integralTable',
+//         name: 'integralTable',
+//         meta: { title: 'integralTable', noCache: true }
+//       },
+//       // 统计分析
+//       {
+//         path: 'statisticalAnalysis',
+//         name: 'statisticalAnalysis',
+//         meta: { title: 'statisticalAnalysis', noCache: true }
+//       }
+//     ]
+//   },
+//   // 系统管理
+//   {
+//     path: '/system',
+//     redirect: '/system/dictionaryManagement',
+//     name: 'system',
+//     meta: {
+//       title: 'system',
+//       icon: 'example'
+//     },
+//     children: [
+//       // 字典管理
+//       {
+//         path: 'dictionaryManagement',
+//         name: 'dictionaryManagement',
+//         meta: { title: 'dictionaryManagement', noCache: true }
+//       },
+//       // 登录管理
+//       {
+//         path: 'loginManagement',
+//         name: 'loginManagement',
+//         meta: { title: 'loginManagement', noCache: true }
+//       },
+//       // 系统日志
+//       {
+//         path: 'systemLog',
+//         name: 'systemLog',
+//         meta: { title: 'systemLog', noCache: true }
+//       },
+//       // 菜单管理
+//       {
+//         path: 'menusManagement',
+//         name: 'menusManagement',
+//         meta: { title: 'menusManagement', noCache: true }
+//       },
+//       // 权限管理
+//       {
+//         path: 'powerManagement',
+//         name: 'powerManagement',
+//         meta: { title: 'powerManagement', noCache: true }
+//       }
+//     ]
+//   }
+//   // icon
+//   // {
+//   //   path: '/icon',
+//   //   children: [
+//   //     {
+//   //       path: 'index',
+//   //       name: 'Icons',
+//   //       meta: { title: 'icons', icon: 'icon', noCache: true }
+//   //     }
+//   //   ]
+//   // }
+//
+//   /** When your routing table is too long, you can split it into small modules**/
+//   //
+//   // { path: '*', redirect: '/404', hidden: true }
+// ]
