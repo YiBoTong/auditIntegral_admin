cf338ca1b3a244c8456a5fb7a7791ad27e26feaf
xilinxia <511598384@qq.com>
2019-01-14 00:24:41 +0800

    Revert "稽核管理-事实确认书：修改查看和管理页完成。"
    This reverts commit 7251c83f6bdaa03988e27180ae19b39d219e048d.

--- src/views/auditManagement/track/index.vue
+++ src/views/auditManagement/track/index.vue
@@ -4,33 +4,16 @@
 ****--@describe 后续检查
 -->
 <template>
-  <div class="public-container">
-    <keep-alive>
-      <index-list
-        v-if="view==='list'"
-        :params-data="paramsData"
-        @view="viewCall" />
-    </keep-alive>
-    <index-show
-      v-if="view==='show'"
-      :params-data="paramsData"
-      @view="viewCall" />
-  </div>
+  <div/>
 </template>
 <script>
 /* 当前组件必要引入 */
-import IndexList from './list'
-import IndexShow from './show'
-
 export default {
   name: 'TrackIndex',
-  components: { IndexList, IndexShow },
+  components: {},
   // props: {},
   data() {
-    return {
-      view: 'list',
-      paramsData: ''
-    }
+    return {}
   },
   // computed: {},
   // watch: {},
@@ -41,11 +24,6 @@ export default {
   methods: {
     // 初始化
     init() {
-    },
-    // 选择显示子组件
-    viewCall(view, data) {
-      this.view = view
-      this.paramsData = data
     }
   }
 }
--- src/views/auditManagement/track/show.vue
+++ src/views/auditManagement/track/show.vue
@@ -4,114 +4,26 @@
 ****--@describe 查看后续检查
 -->
 <template>
-  <el-card v-loading="dataLoading" class="track-show-container">
-    <el-row slot="header">
-      <el-col align="right">
-        <el-button type="text" @click="backList">返回列表</el-button>
-      </el-col>
-      <el-col align="center">
-        <h1>{{ this.$store.getters["com"]('name') }}XX年XXX专项审计后续检查表</h1>
-      </el-col>
-    </el-row>
-    <div class="card-content">
-      <div class="public-div"><span>检查单位：兴仁农商银行稽核审计部</span></div>
-      <div class="public-div two-row"><el-col :span="12">检查项目：XXX 年XXX专项审计</el-col><el-col :span="12">检查内容：信贷资料</el-col></div>
-      <div class="public-div"><span>检查时间：XXXX年XXX月XXX日</span></div>
-      <div class="public-div four-row"><el-col :span="12">检查人：</el-col><el-col :span="12">复核人：</el-col></div>
-      <div class="table">
-        <el-table
-          :data="tableData"
-          border
-          show-summary
-          style="width: 99.99%">
-          <el-table-column
-            type="index"
-            label="序号"
-          />
-          <el-table-column
-            prop="name"
-            label="客户名称"/>
-          <el-table-column
-            prop=""
-            sortable
-            label="贷款日期"/>
-          <el-table-column
-            prop=""
-            sortable
-            label="到期日期"/>
-          <el-table-column
-            prop="num1"
-            sortable
-            label="贷款金额"/>
-          <el-table-column
-            prop="num2"
-            sortable
-            label="结欠金额"/>
-          <el-table-column
-            prop=""
-            sortable
-            label="存在问题"/>
-          <el-table-column
-            prop=""
-            sortable
-            label="整改情况"/>
-          <el-table-column
-            prop=""
-            sortable
-            label="被检查单位"/>
-        </el-table>
-      </div>
-      <div class="public-div"><span>被检查单位意见：</span></div>
-      <div class="public-div"><span>被检查单位负责人（或授权代理人）签字：</span></div>
-      <div class="public-div"><span>年月日</span></div>
-    </div>
-  </el-card>
+  <div/>
 </template>
 <script>
 /* 当前组件必要引入 */
-// import { } from '@/api/auditManagement'
-
 export default {
   name: 'TrackShow',
   components: {},
-  props: {
-    paramsData: {
-      type: [Object, String],
-      required: false,
-      default: ''
-    }
-  },
+  // props: {},
   data() {
-    return {
-      dataLoading: false,
-      tableData: [
-        {
-          num1: 12,
-          num2: 15
-        },
-        {
-          num1: 20,
-          num2: 1456
-        }
-      ]
-    }
+    return {}
   },
+  // computed: {},
+  // watch: {},
   created() {
     this.init()
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     // 初始化
     init() {
-      if (this.paramsData) {
-        console.log(this.paramsData)
-        // const id = this.paramsData.id
-      }
-    },
-    // 返回列表
-    backList() {
-      this.$emit('view', 'list')
     }
   }
 }
